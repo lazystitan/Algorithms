9c378ab92d228d6821284f47be4a96620b50a8ce
Riton <ritonelion@outlook.com>
2019-06-11 16:14:44 +0800

    修改了Kmp::search的bug

--- src/kmp.rs
+++ src/kmp.rs
@@ -8,8 +8,13 @@ impl Kmp {
     pub fn new(pattern : String) -> Kmp{
         let pattern_as_bytes = pattern.as_bytes();
         let len = pattern.len();
+
+        eprintln!("{}",len);
+
         let height = 256;
         let mut dfa:Vec<Vec<usize>> = vec![vec![0;len];height];
+
+        eprintln!("{:?}",dfa[0]);
 
         let mut x = 0;
         let mut c= pattern_as_bytes[0] as usize;
@@ -43,9 +48,18 @@ impl Kmp {
         let mut j = 0;
         let mut position= 0;
         let text_as_bytes = text.as_bytes();
+
+//        for line in &self.dfa {
+//            eprintln!("{:?}",line);
+//        }
+
         for i in 0..text.len() {
+            eprintln!("{}",j);
             j = self.dfa[text_as_bytes[i] as usize][j] as usize;
             position = i;
+            if j >= self.pattern.len() {
+                break;
+            }
         }
         if j == self.pattern.len() {
             position - self.pattern.len() + 1
@@ -61,20 +75,31 @@ mod test {
 
     #[test]
     fn search_test() {
-        let text = "acabc".to_string();
-        let pattern = "abc".to_string();
+//        let text = "acabc".to_string();
+//        let pattern = "abc".to_string();
+//
+//        let kmp = Kmp::new(pattern);
+//        let position = kmp.search(&text);
+//
+//        assert_eq!(position, 2);
+//
+//        let pattern = "abcd".to_string();
+//
+//        let kmp = Kmp::new(pattern);
+//        let position = kmp.search(&text);
+//
+//        assert_eq!(position, text.len());
+
+        let text = "where am i? what the fuck is this?".to_string();
+        let pattern = "what".to_string();
+
+        eprintln!("{}",pattern.len());
 
         let kmp = Kmp::new(pattern);
+        kmp.show_dfa();
         let position = kmp.search(&text);
 
-        assert_eq!(position, 2);
-
-        let pattern = "abcd".to_string();
-
-        let kmp = Kmp::new(pattern);
-        let position = kmp.search(&text);
-
-        assert_eq!(position, text.len());
+        assert_eq!(position, 12);
 
     }
 
